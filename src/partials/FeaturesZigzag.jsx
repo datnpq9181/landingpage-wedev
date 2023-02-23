0eb6fc484bdc026e102af3add1292738387fa92d
Ngo Dat <ngodat@Ngos-MacBook-Pro.local>
2023-02-23 13:36:36 +0700

    1st commit

---
+++ src/partials/FeaturesZigzag.jsx
@@ -0,0 +1,139 @@
+import React from 'react';
+
+import FeatImage01 from '../images/features-03-image-01.png';
+import FeatImage02 from '../images/features-03-image-02.png';
+import FeatImage03 from '../images/features-03-image-03.png';
+
+function FeaturesZigzag() {
+  return (
+    <section>
+      <div className="max-w-6xl mx-auto px-4 sm:px-6">
+        <div className="py-12 md:py-20 border-t border-gray-800">
+
+          {/* Section header */}
+          <div className="max-w-3xl mx-auto text-center pb-12 md:pb-16">
+            <div className="inline-flex text-sm font-semibold py-1 px-3 m-2 text-green-600 bg-green-200 rounded-full mb-4">Sản phẩm nổi bật của Wedev Mobi</div>
+            <h1 className="h2 mb-4">Một phần mềm, không giới hạn giải pháp</h1>
+            <p className="text-xl text-gray-400">Trọn bộ giải pháp chuyển đổi số phân mục quản lý tài chính cho doanh nghiệp.</p>
+          </div>
+
+          {/* Items */}
+          <div className="grid gap-20">
+
+            {/* 1st item */}
+            <div className="md:grid md:grid-cols-12 md:gap-6 items-center">
+              {/* Image */}
+              <div className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-5 lg:col-span-6 mb-8 md:mb-0 md:order-1" data-aos="fade-up">
+                <img className="max-w-full mx-auto md:max-w-none h-auto" src={FeatImage01} width="540" height="405" alt="Features 01" />
+              </div>
+              {/* Content */}
+              <div className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-7 lg:col-span-6" data-aos="fade-right">
+                <div className="md:pr-4 lg:pr-12 xl:pr-16">
+                  <div className="font-architects-daughter text-xl text-purple-600 mb-2">More speed. Less spend</div>
+                  <h3 className="h3 mb-3">Open Banking API ngân hàng</h3>
+                  <p className="text-xl text-gray-400 mb-4">Tự động đồng bộ giao dịch của các tài khoản ngân hàng mà bạn đã liên kết với hệ thống DnD Pro.</p>
+                  <ul className="text-lg text-gray-400 -mb-2">
+                    <li className="flex items-center mb-2">
+                      <svg className="w-3 h-3 fill-current text-green-500 mr-2 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
+                      </svg>
+                      <span>Tiết kiệm thời gian và nguồn lực</span>
+                    </li>
+                    <li className="flex items-center mb-2">
+                      <svg className="w-3 h-3 fill-current text-green-500 mr-2 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
+                      </svg>
+                      <span>Nhanh chóng theo dõi tổng quan tổng quan hoạt động</span>
+                    </li>
+                    <li className="flex items-center">
+                      <svg className="w-3 h-3 fill-current text-green-500 mr-2 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
+                      </svg>
+                      <span>Nguồn dữ liệu quan trọng nhằm đánh giá sức khỏe tài chính doanh nghiệp</span>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+
+            {/* 2nd item */}
+            <div className="md:grid md:grid-cols-12 md:gap-6 items-center">
+              {/* Image */}
+              <div className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-5 lg:col-span-6 mb-8 md:mb-0 rtl" data-aos="fade-up">
+                <img className="max-w-full mx-auto md:max-w-none h-auto" src={FeatImage02} width="540" height="405" alt="Features 02" />
+              </div>
+              {/* Content */}
+              <div className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-7 lg:col-span-6" data-aos="fade-left">
+                <div className="md:pl-4 lg:pl-12 xl:pl-16">
+                  <div className="font-architects-daughter text-xl text-purple-600 mb-2">Do less. Think more</div>
+                  <h3 className="h3 mb-3">Chatbot tự động</h3>
+                  <p className="text-xl text-gray-400 mb-4">Tự động nhắn tin giao dịch mới đến các nhóm chat</p>
+                  <ul className="text-lg text-gray-400 -mb-2">
+                    <li className="flex items-center mb-2">
+                      <svg className="w-3 h-3 fill-current text-green-500 mr-2 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
+                      </svg>
+                      <span>Hỗ trợ các nền tảng phổ biến như Zalo, Telegram ...</span>
+                    </li>
+                    <li className="flex items-center mb-2">
+                      <svg className="w-3 h-3 fill-current text-green-500 mr-2 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
+                      </svg>
+                      <span>Tài liệu tích hợp cụ thể, rõ ràng, dễ thao tác</span>
+                    </li>
+                    <li className="flex items-center">
+                      <svg className="w-3 h-3 fill-current text-green-500 mr-2 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
+                      </svg>
+                      <span>Cập nhật giao dịch liên tục 24/7</span>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+
+            {/* 3rd item */}
+            <div className="md:grid md:grid-cols-12 md:gap-6 items-center">
+              {/* Image */}
+              <div className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-5 lg:col-span-6 mb-8 md:mb-0 md:order-1" data-aos="fade-up">
+                <img className="max-w-full mx-auto md:max-w-none h-auto" src={FeatImage03} width="540" height="405" alt="Features 03" />
+              </div>
+              {/* Content */}
+              <div className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-7 lg:col-span-6" data-aos="fade-right">
+                <div className="md:pr-4 lg:pr-12 xl:pr-16">
+                  <div className="font-architects-daughter text-xl text-purple-600 mb-2">Do more. Think less</div>
+                  <h3 className="h3 mb-3">Quản lý Thu Chi</h3>
+                  <p className="text-xl text-gray-400 mb-4">tự động gán nhãn cho giao dịch, dễ dàng phân loại</p>
+                  <ul className="text-lg text-gray-400 -mb-2">
+                    <li className="flex items-center mb-2">
+                      <svg className="w-3 h-3 fill-current text-green-500 mr-2 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
+                      </svg>
+                      <span>Giao diện thân thuộc, sử dụng y hệt Microsoft Excel</span>
+                    </li>
+                    <li className="flex items-center mb-2">
+                      <svg className="w-3 h-3 fill-current text-green-500 mr-2 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
+                      </svg>
+                      <span>Chủ động đặt quy tắc phân loại, phân luồng giao dịch</span>
+                    </li>
+                    <li className="flex items-center">
+                      <svg className="w-3 h-3 fill-current text-green-500 mr-2 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
+                      </svg>
+                      <span>Tự động hóa quy trình với độ chính xác cao</span>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+
+          </div>
+
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default FeaturesZigzag;
---
+++ src/partials/Testimonials.jsx
@@ -0,0 +1,78 @@
+import React from 'react';
+
+import TestimonialImage01 from '../images/testimonial-01.jpg';
+import TestimonialImage02 from '../images/testimonial-02.jpg';
+import TestimonialImage03 from '../images/testimonial-03.jpg';
+
+function Testimonials() {
+  return (
+    <section>
+      <div className="max-w-6xl mx-auto px-4 sm:px-6">
+        <div className="py-12 md:py-20 border-t border-gray-800">
+
+          {/* Section header */}
+          <div className="max-w-3xl mx-auto text-center pb-12 md:pb-20">
+            <h2 className="h2 mb-4">Vẫn còn băn khoăn một chút, không sao</h2>
+            <p className="text-xl text-gray-400">Khách hàng đã sử dụng và đánh giá về DnD Pro</p>
+          </div>
+
+          {/* Testimonials */}
+          <div className="max-w-sm mx-auto grid gap-8 lg:grid-cols-3 lg:gap-6 items-start lg:max-w-none">
+
+            {/* 1st testimonial */}
+            <div className="flex flex-col h-full p-6 bg-gray-800" data-aos="fade-up">
+              <div>
+                <div className="relative inline-flex flex-col mb-4">
+                  <img className="rounded-full" src={TestimonialImage01} width="48" height="48" alt="Testimonial 01" />
+                    <svg className="absolute top-0 right-0 -mr-3 w-6 h-5 fill-current text-purple-600" viewBox="0 0 24 20" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M0 13.517c0-2.346.611-4.774 1.833-7.283C3.056 3.726 4.733 1.648 6.865 0L11 2.696C9.726 4.393 8.777 6.109 8.152 7.844c-.624 1.735-.936 3.589-.936 5.56v4.644H0v-4.531zm13 0c0-2.346.611-4.774 1.833-7.283 1.223-2.508 2.9-4.586 5.032-6.234L24 2.696c-1.274 1.697-2.223 3.413-2.848 5.148-.624 1.735-.936 3.589-.936 5.56v4.644H13v-4.531z" />
+                    </svg>
+                        </div>
+                </div>
+                <blockquote className="text-lg text-gray-400 grow">— DnD PRO đơn giản, dễ sử dụng mà hiệu quả cao. Không nói ngoa đâu, mình có đến 5 tài khoản ngân hàng khác nhau, mỗi lần khách chuyển tiền xong cứ phải nhờ khách chụp ảnh chuyển tiền thành công, xong lại ngồi chờ tiền về. Bây giờ thì không cần nữa rồi, hầu như mình nhận được thông báo ngay lập tức từ DnD Pro ngay khi khách chuyển tiền thành công.</blockquote>
+                <div className="text-gray-700 font-medium mt-6 pt-5 border-t border-gray-700">
+                  <cite className="text-gray-200 not-italic">Ms. Yên - COO</cite> - <a className="text-purple-600 hover:text-gray-200 transition duration-150 ease-in-out" href="#0">Patronus Jewelry</a>
+                </div>
+              </div>
+
+              {/* 2nd testimonial */}
+              <div className="flex flex-col h-full p-6 bg-gray-800" data-aos="fade-up" data-aos-delay="200">
+                <div>
+                  <div className="relative inline-flex flex-col mb-4">
+                    <img className="rounded-full" src={TestimonialImage02} width="48" height="48" alt="Testimonial 02" />
+                      <svg className="absolute top-0 right-0 -mr-3 w-6 h-5 fill-current text-purple-600" viewBox="0 0 24 20" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M0 13.517c0-2.346.611-4.774 1.833-7.283C3.056 3.726 4.733 1.648 6.865 0L11 2.696C9.726 4.393 8.777 6.109 8.152 7.844c-.624 1.735-.936 3.589-.936 5.56v4.644H0v-4.531zm13 0c0-2.346.611-4.774 1.833-7.283 1.223-2.508 2.9-4.586 5.032-6.234L24 2.696c-1.274 1.697-2.223 3.413-2.848 5.148-.624 1.735-.936 3.589-.936 5.56v4.644H13v-4.531z" />
+                      </svg>
+                        </div>
+                  </div>
+                  <blockquote className="text-lg text-gray-400 grow">— Thực sự ban đầu mình hơi lo lắng vì phải nhập thông tin đăng nhập ngân hàng điện tử khi liên kết với DnD Pro, nhưng sau khi được các bạn nhân viên tư vấn, thậm chí còn hỗ trợ triển khai thực tế lên shop của mình, mình hoàn toàn hài lòng vì hiệu quả mà công cụ này mang lại, không còn lo lắng gì về vấn đề bảo mật nữa!</blockquote>
+                  <div className="text-gray-700 font-medium mt-6 pt-5 border-t border-gray-700">
+                    <cite className="text-gray-200 not-italic">Ms. Huyền - COO</cite> - <a className="text-purple-600 hover:text-gray-200 transition duration-150 ease-in-out" href="#0">SIM Boutique</a>
+                  </div>
+                </div>
+
+                {/* 3rd testimonial */}
+                <div className="flex flex-col h-full p-6 bg-gray-800" data-aos="fade-up" data-aos-delay="400">
+                  <div>
+                    <div className="relative inline-flex flex-col mb-4">
+                      <img className="rounded-full" src={TestimonialImage03} width="48" height="48" alt="Testimonial 03" />
+                        <svg className="absolute top-0 right-0 -mr-3 w-6 h-5 fill-current text-purple-600" viewBox="0 0 24 20" xmlns="http://www.w3.org/2000/svg">
+                          <path d="M0 13.517c0-2.346.611-4.774 1.833-7.283C3.056 3.726 4.733 1.648 6.865 0L11 2.696C9.726 4.393 8.777 6.109 8.152 7.844c-.624 1.735-.936 3.589-.936 5.56v4.644H0v-4.531zm13 0c0-2.346.611-4.774 1.833-7.283 1.223-2.508 2.9-4.586 5.032-6.234L24 2.696c-1.274 1.697-2.223 3.413-2.848 5.148-.624 1.735-.936 3.589-.936 5.56v4.644H13v-4.531z" />
+                        </svg>
+                        </div>
+                    </div>
+                    <blockquote className="text-lg text-gray-400 grow">— Tiện và lợi cho người sử dụng. Giao diện đơn giản, không quá màu mè nhưng thẳng vấn đề, hiệu quả cho việc quản lý dòng tiền. Mình đã áp dụng thử 01 tháng cho chuỗi 20 cửa hàng bán lẻ, đánh giá cao tính khả thi của phần mềm này.</blockquote>
+                    <div className="text-gray-700 font-medium mt-6 pt-5 border-t border-gray-700">
+                      <cite className="text-gray-200 not-italic">Mr. Austin Ngô - CEO</cite> - <a className="text-purple-600 hover:text-gray-200 transition duration-150 ease-in-out" href="#0">CMN Vietnam</a>
+                    </div>
+                  </div>
+
+                </div>
+
+              </div>
+            </div>
+</section>
+  );
+}
+
+export default Testimonials;
